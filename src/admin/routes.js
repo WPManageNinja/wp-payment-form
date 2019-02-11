afcf259fb1576ea4e03e58a43e9118ec1454a6ca
Shahjahan Jewel <cep.jewel@gmail.com>
2019-02-11 12:00:50 +0600

    rearrange routes

--- src/admin/routes.js
+++ src/admin/routes.js
@@ -2,13 +2,10 @@ const DashBoardStats = require('./Components/Dashboard');
 const GlobalView = require('./Components/Global/index');
 const AllForms = require('./Components/Forms/AllForms');
 const EditFormView = require('./Components/Form/index');
-const EditSingleForm = require('./Components/Dashboard');
 
-const FormSettingsIndex = require('./Components/Form/settings/index');
 const FormBuilder = require('./Components/Form/FormBuilder');
 const FormPaymentSettings = require('./Components/Form/settings/ConfirmationSettings');
 const FormCurrencySettings = require('./Components/Form/settings/CurrencySettings');
-
 
 import Entries from './Components/Entries/Entries';
 import Entry from './Components/Entries/Entry';
@@ -16,6 +13,42 @@ import SettingView from './Components/Settings/index'
 import StripeSettings from './Components/Settings/StripeSettings'
 import GeneralSettings from './Components/Settings/GeneralSettings'
 import FormDesignSettings from './Components/Form/settings/FormDesignSettings'
+
+const formEditorChildrenRoutes = window.WPPayForms.applyFilters('wpf_form_children_roues', [
+    {
+        path: 'form-builder',
+        name: 'edit_form',
+        component: FormBuilder
+    },
+    {
+        path: 'confirmation_settings',
+        name: 'confirmation_settings',
+        component: FormPaymentSettings
+    },
+    {
+        path: 'currency_settings',
+        name: 'form_currency_settings',
+        component: FormCurrencySettings
+    },
+    {
+        path: 'design_options',
+        name: 'design_options',
+        component: FormDesignSettings
+    }
+]);
+
+const globalSettingsViewChilderRoutes = window.WPPayForms.applyFilters('wpf_global_settings_childern_routes', [
+    {
+        name: 'stripe_settings',
+        path: 'stripe-settings',
+        component: StripeSettings
+    },
+    {
+        name: 'general_settings',
+        path: 'general-settings',
+        component: GeneralSettings
+    }
+]);
 
 export const routes = [
     {
@@ -41,62 +74,19 @@ export const routes = [
             {
                 path: '/settings/',
                 component: SettingView,
-                children: [
-                    {
-                        name:'stripe_settings',
-                        path: 'stripe-settings',
-                        component: StripeSettings
-                    },
-                    {
-                        name:'general_settings',
-                        path: 'general-settings',
-                        component: GeneralSettings
-                    }
-                ]
+                children: globalSettingsViewChilderRoutes
             },
             {
                 path: '/support',
                 name: 'support',
                 component: DashBoardStats
-            },
+            }
         ]
     },
     {
-        path: '/edit-form/:form_id/',
+        path: '/edit-form/:form_id/settings/',
         component: EditFormView,
         props: true,
-        children: [
-            {
-                path: 'form-builder',
-                name: 'edit_form',
-                component: FormBuilder
-            },
-            {
-                path: 'settings/',
-                component: FormSettingsIndex,
-                children: [
-                    {
-                        path: 'confirmation_settings',
-                        name: 'confirmation_settings',
-                        component: FormPaymentSettings
-                    },
-                    {
-                        path: 'currency_settings',
-                        name: 'form_currency_settings',
-                        component: FormCurrencySettings
-                    },
-                    {
-                        path: 'design_options',
-                        name: 'design_options',
-                        component: FormDesignSettings
-                    },
-                    {
-                        path: 'email_settings',
-                        name: 'email_settings',
-                        component: EditSingleForm
-                    }
-                ]
-            }
-        ]
+        children: formEditorChildrenRoutes
     }
 ];
