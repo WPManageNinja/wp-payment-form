f4df22e59f1e1c79689cd9f8f9b669e1fa4498bc
Shahjahan Jewel <cep.jewel@gmail.com>
2019-02-14 17:07:03 +0600

    fix

--- wp-payment-form.php
+++ wp-payment-form.php
@@ -163,9 +163,9 @@ class WPPayForm
     public function loadDependecies()
     {
         require_once(WPPAYFORM_DIR . 'includes/autoload.php');
-//        if(file_exists(WPPAYFORM_DIR.'includes/Pro/init.php')) {
-//            require_once WPPAYFORM_DIR.'includes/Pro/init.php';
-//        }
+        if(file_exists(WPPAYFORM_DIR.'includes/Pro/init.php')) {
+            require_once WPPAYFORM_DIR.'includes/Pro/init.php';
+        }
     }
 }
 
